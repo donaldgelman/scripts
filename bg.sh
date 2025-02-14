0432fd379529a734de3417f1deda83873a21d19a
donaldgelman <donaldgelman2@gmail.com>
2025-02-14 09:14:16 -0400

    bg_remove

--- bg.sh
+++ bg.sh
@@ -1,5 +1,26 @@
 #!/bin/bash
 
-randpic=$(shuf -n 1 ~/bin/bg/my_photos.txt)
-echo "${randpic%.*}" | grep -Eo "\b[^/]*$" > ~/bin/bg/bgtmp.txt
-feh --bg-max $randpic
+current=$HOME/bin/bg/bgtmp.txt
+myphotos=$HOME/bin/bg/my_photos.txt
+
+randpic=$(shuf -n 1 $myphotos)
+echo "$randpic" > $current
+
+bg() {
+	feh --bg-max $randpic
+}
+
+bg_remove() {
+	grep -vFf "$current" $myphotos > temp.txt && mv temp.txt $myphotos
+}
+
+if [[ $# -eq 0 ]]; then
+    bg
+else
+    if [[ "$1" == "remove" ]]; then
+        bg_remove
+    else
+        echo "Invalid command. Use 'bg' or 'bg remove'."
+        exit 1
+    fi
+fi
